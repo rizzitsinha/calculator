9e1fa879ab3ecd13448a98905811b9762d026b2f
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-17 17:16:24 +0530

    Digit buttons value append onto the display, and the display value is stored in a variable as a number

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ let operator;
 function operate(firstOperand, secondOperand, operator){
     return operator(firstOperand, secondOperand);
 }
+
+const digitButtons = document.querySelectorAll(".digit-btn");
+const display = document.querySelector(".display-value");
+
+let text = ""
+display.textContent = text;
+let displayValue;
+
+digitButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let num = e.target.id;
+        console.log(num);
+        text += num;
+        display.textContent = text;
+        displayValue = +text;
+        console.log(displayValue)
+
+    })
+})
+
